7f8dc8026fccfc84732a591a09bf979c5d59aaa2
Pranay Tripathi <pranaytripathi123@gmail.com>
2025-07-27 17:09:35 +0100

    Adding login module and static serving

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"api-endpoints/templates"
+	"api-endpoints/src/login"
+	"api-endpoints/src/templates"
 	"fmt"
 	"github.com/a-h/templ"
 	"log"
@@ -9,7 +10,13 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.Handle("/", templ.Handler(templates.Index()))
+	http.Handle("/login", templ.Handler(templates.LoginPage()))
+
+	http.HandleFunc("/api/login", login.Handler)
 	fmt.Println("Server running at http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
---
+++ src/login/login.go
@@ -0,0 +1,10 @@
+package login
+
+import "net/http"
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+}
